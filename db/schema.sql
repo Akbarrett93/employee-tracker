19f3be4ca6557efc6702dec0f4d48f9240a22364
Alex Barrett <akbarrett1993@gmail.com>
2023-02-20 16:28:09 -0500

    schema made, seeds added to test

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,22 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE
+CREATE TABLE department(
+    id INT NOT NULL PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT REFERENCES role(id),
+    manager_id INT REFERENCES employees(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (id, dep_name)
+VALUES  (1, "HR"),
+        (2, "IT"),
+        (3, "R&D"),
+        (4, "Production");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES  (1, "HR Generalist", 75000, 1),
+        (2, "IT Manager", 70000, 2),
+        (3, "Research Scientist", 90000, 3),
+        (4, "Production Manager", 65000, 4),
+        (5, "Lab Technician", 50000, 3),
+        (6, "Production Associate", 45000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (1, "Paul", "Coffey", 4, NULL),
+        (2, "Octavious", "Moore", 6, 4);
